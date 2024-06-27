5cc1952c9669fa1a01fbf01e44150ee977c3ed75
nadeen <nadeenserag@gmail.com>
2024-06-27 15:54:40 +0300

    bottom menu

--- app/src/main/java/com/example/masterchef/onboarding/SplashScreenActivity.kt
+++ app/src/main/java/com/example/masterchef/onboarding/SplashScreenActivity.kt
@@ -1,8 +1,8 @@
 package com.example.masterchef.onboarding
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.masterchef.R
 
@@ -10,9 +10,26 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_splash_screen)
-        startActivity(Intent(this, OnBoardingActivity::class.java))
-        finish()
+//        setContentView(R.layout.activity_splash_screen) // causes error
 
+        startActivity(Intent(this@SplashScreenActivity, OnBoardingActivity::class.java))
+        finish()
+//        val animationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
+//
+//        animationView.addAnimatorListener(object : Animator.AnimatorListener {
+//            override fun onAnimationStart(animation: Animator) {
+//                animationView.setAnimation(R.raw.food)
+//                animationView.playAnimation()
+//            }
+//
+//            override fun onAnimationEnd(animation: Animator) {
+//                startActivity(Intent(this@SplashScreenActivity, OnBoardingActivity::class.java))
+//                finish()
+//            }
+//
+//            override fun onAnimationCancel(animation: Animator) {}
+//
+//            override fun onAnimationRepeat(animation: Animator) {}
+//        })
     }
 }
